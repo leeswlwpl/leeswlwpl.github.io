4acc95b0e0db16c53112528d6842d5e1786cae7f
Anthony Lee <leeswlwpl@gmail.com>
2023-08-28 17:45:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-    console.log("main.js begin!");
+// $(document).ready(function () {
+//     console.log("main.js begin!");
+// }
+
+function init(){
+    console.log("main.js init begin!")
 }
+
+window.addEventListener('load', function(){
+    init();
+});
